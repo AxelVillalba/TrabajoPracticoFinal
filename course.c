0ed1ac456c12f7ecdac45cfd01f93a3acf4c9e42
Alexander Chavez <jylalexander1007@gmail.com>
2025-11-20 19:53:04 -0300

    implementar operaciones CRUD para materias

--- course.c
+++ course.c
@@ -39,3 +39,78 @@ static int course_list_resize(CourseList *list) {
     }
     return 0;
 }
+int course_list_add(CourseList *list, const char *name, int credits) {
+    if (!list || !name) return -1;
+    
+    if (list->size >= list->capacity) {
+        if (!course_list_resize(list)) {
+            return -1;
+        }
+    }
+    
+    Course *course = &list->data[list->size];
+    course->id = next_course_id++;
+    strncpy(course->name, name, MAX_COURSE_NAME_LENGTH - 1);
+    course->name[MAX_COURSE_NAME_LENGTH - 1] = '\0';
+    course->credits = credits;
+    
+    list->size++;
+    return course->id;
+}
+
+int course_list_remove(CourseList *list, int id) {
+    if (!list) return 0;
+    
+    for (int i = 0; i < list->size; i++) {
+        if (list->data[i].id == id) {
+            for (int j = i; j < list->size - 1; j++) {
+                list->data[j] = list->data[j + 1];
+            }
+            list->size--;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+Course* course_list_find_by_id(CourseList *list, int id) {
+    if (!list) return NULL;
+    
+    for (int i = 0; i < list->size; i++) {
+        if (list->data[i].id == id) {
+            return &list->data[i];
+        }
+    }
+    return NULL;
+}
+
+void course_list_display(CourseList *list) {
+    if (!list || list->size == 0) {
+        printf("\nNo hay materias registradas.\n");
+        return;
+    }
+    
+    printf("\n=== LISTA DE MATERIAS ===\n");
+    printf("%-5s %-40s %-10s\n", "ID", "Nombre", "Creditos");
+    printf("--------------------------------------------------------------\n");
+    
+    for (int i = 0; i < list->size; i++) {
+        Course *c = &list->data[i];
+        printf("%-5d %-40s %-10d\n", c->id, c->name, c->credits);
+    }
+    printf("\n");
+}
+
+int course_list_update(CourseList *list, int id, const char *new_name, int new_credits) {
+    Course *course = course_list_find_by_id(list, id);
+    if (!course) return 0;
+    
+    if (new_name) {
+        strncpy(course->name, new_name, MAX_COURSE_NAME_LENGTH - 1);
+        course->name[MAX_COURSE_NAME_LENGTH - 1] = '\0';
+    }
+    if (new_credits >= 0) {
+        course->credits = new_credits;
+    }
+    return 1;
+}
